5e09e96fe62c17a8dc5a0d6eb32b5acc9f567341
ronaldobustosvega75 <ronaldobustosvega75@gmail.com>
2025-10-07 23:21:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ st.set_page_config(page_title="Procesamiento de Imágenes", page_icon="🎨", la
 hide_streamlit_style = """
     <style>
     header {visibility: hidden;}
-    /* Mantener visible SOLO el botón del sidebar */
-    button[kind="header"] {
-        visibility: visible !important;
-    }
     .stDeployButton {display:none;}
     .viewerBadge_container__1QSob {display: none;}
     
@@ -26,6 +22,7 @@ Explora el procesamiento de imágenes con OpenCV.
 **Navega usando el menú lateral ⬅️**
 """)
 st.info("👈 Selecciona un capítulo del menú lateral para comenzar")
+
 
 
 

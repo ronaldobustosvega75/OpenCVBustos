42f31204d7cd57d764567abb110f70a6aa69462d
ronaldobustosvega75 <ronaldobustosvega75@gmail.com>
2025-10-07 23:02:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Procesamiento de Imágenes", page_icon="🎨", layout="wide")
 
-hide_streamlit_style = """
-    <style>
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
-    [data-testid="stToolbar"] {display: none;}
-    #MainMenu {visibility: visible !important;}
-    [data-testid="collapsedControl"] {display: block !important;}
-    </style>
-"""
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 
 st.title("🎨 Procesamiento Digital de Imágenes")
 st.markdown("""
@@ -20,6 +11,7 @@ Explora el procesamiento de imágenes con OpenCV.
 **Navega usando el menú lateral ⬅️**
 """)
 st.info("👈 Selecciona un capítulo del menú lateral para comenzar")
+
 
 
 
